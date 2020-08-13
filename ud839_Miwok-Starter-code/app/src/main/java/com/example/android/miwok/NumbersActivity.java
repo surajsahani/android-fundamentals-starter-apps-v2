ac16b050a40fdf5f9829bfee0c0148d073079f4c
Suraj sahani <surajkumarsahani1997@gmail.com>
2020-08-13 16:25:43 +0530

    stash

--- ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ ud839_Miwok-Starter-code/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,7 +3,9 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -1018,6 +1020,12 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("Nine hundred Ninety-Eight");
         words.add("Nine hundred Ninety-Nine");
         words.add("One Thousand");
+
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
 
         // find the root view of the whole layout
         LinearLayout rootView =(LinearLayout)findViewById(R.id.rootView);
